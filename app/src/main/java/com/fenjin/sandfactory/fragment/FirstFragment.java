a2ecb4eab3bf603145536cb7d1adafc48b36a18e
孔国光 <kgg@xdja.com>
2018-08-22 17:14:06 +0800

    错误码401退出到登录界面

--- app/src/main/java/com/fenjin/sandfactory/fragment/FirstFragment.java
+++ app/src/main/java/com/fenjin/sandfactory/fragment/FirstFragment.java
@@ -3,6 +3,8 @@ package com.fenjin.sandfactory.fragment;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,17 +12,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
-import com.fenjin.data.entity.ChengZhongRecord;
+import com.blankj.utilcode.util.LogUtils;
 import com.fenjin.sandfactory.R;
-import com.fenjin.sandfactory.adapter.ChengZhongListAdapter;
+import com.fenjin.sandfactory.activity.LoginActivity;
+import com.fenjin.sandfactory.databinding.FragmentFirstBinding;
+import com.fenjin.sandfactory.util.ErrorCodeUtil;
 import com.fenjin.sandfactory.viewmodel.FirstViewModel;
-import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.qmuiteam.qmui.widget.dialog.QMUITipDialog;
 import com.qmuiteam.qmui.widget.pullRefreshLayout.QMUIPullRefreshLayout;
-
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,10 +34,6 @@ public class FirstFragment extends Fragment {
 
     private FirstViewModel viewModel;
 
-    private ListView listView;
-
-    private ChengZhongListAdapter adapter;
-
     private QMUITipDialog loadingDialog;
 
     private QMUIPullRefreshLayout pullRefreshLayout;
@@ -46,8 +42,7 @@ public class FirstFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = ViewModelProviders.of(this).get(FirstViewModel.class);
-        adapter = new ChengZhongListAdapter();
-        init();
+        registerObserver();
     }
 
 
@@ -55,15 +50,15 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        FragmentFirstBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        binding.setViewModel(viewModel);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        listView = view.findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
         pullRefreshLayout = view.findViewById(R.id.layout_pull_refresh);
         pullRefreshLayout.setOnPullListener(new QMUIPullRefreshLayout.OnPullListener() {
             @Override
@@ -83,18 +78,7 @@ public class FirstFragment extends Fragment {
         viewModel.loadChengZhongRecordList();
     }
 
-    private void init(){
-        viewModel.chengZhongRecordListLive.observe(this, new Observer<List<ChengZhongRecord>>() {
-            @Override
-            public void onChanged(@Nullable List<ChengZhongRecord> chengZhongRecords) {
-                if (chengZhongRecords != null){
-                    adapter.setChengZhongRecordList(chengZhongRecords);
-                    adapter.notifyDataSetChanged();
-                }
-
-
-            }
-        });
+    private void registerObserver(){
 
         viewModel.loading.observe(this, new Observer<Boolean>() {
             @Override
@@ -114,6 +98,19 @@ public class FirstFragment extends Fragment {
                     if (loadingDialog != null && loadingDialog.isShowing()){
                         loadingDialog.dismiss();
                     }
+                }
+            }
+        });
+
+        viewModel.errorCode.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer integer) {
+
+                if (integer == null) return;
+
+                if (integer == ErrorCodeUtil.TOKEN_TIME_OUT){
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/fenjin/sandfactory/viewmodel/FirstViewModel.java
+++ app/src/main/java/com/fenjin/sandfactory/viewmodel/FirstViewModel.java
@@ -2,11 +2,14 @@ package com.fenjin.sandfactory.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.MutableLiveData;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 
 import com.fenjin.data.entity.ChengZhongRecord;
 import com.fenjin.data.entity.ChengZhongRecordListResult;
+import com.fenjin.sandfactory.adapter.ChengZhongListAdapter;
 import com.fenjin.sandfactory.usecase.GetChengZhongrecordListUseCase;
+import com.fenjin.sandfactory.util.ErrorCodeUtil;
 
 import java.util.List;
 
@@ -18,11 +21,18 @@ public class FirstViewModel extends BaseViewModel {
         super(application);
     }
 
-    public MutableLiveData<List<ChengZhongRecord>> chengZhongRecordListLive = new MutableLiveData<>();
 
     private GetChengZhongrecordListUseCase useCase = new GetChengZhongrecordListUseCase(getApplication());
 
     public MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+    public MutableLiveData<Integer> errorCode = new MutableLiveData<>();
+
+    public ObservableField<String> totalTruckNumber = new ObservableField<>();
+
+    public ObservableField<String> totalWeight = new ObservableField<>();
+
+    public ChengZhongListAdapter adapter = new ChengZhongListAdapter();
 
     public void loadChengZhongRecordList(){
         loading.postValue(true);
@@ -37,14 +47,15 @@ public class FirstViewModel extends BaseViewModel {
                     public void onNext(ChengZhongRecordListResult chengZhongRecordListResult) {
                         loading.postValue(false);
                         if (chengZhongRecordListResult.getFlag() == 1){
-                            chengZhongRecordListLive.postValue(
-                                    chengZhongRecordListResult.getResult().getList());
+                            adapter.setChengZhongRecordList(chengZhongRecordListResult.getResult().getList());
+                            adapter.notifyDataSetChanged();
                         }
                     }
 
                     @Override
                     public void onError(Throwable e) {
                         loading.postValue(false);
+                        errorCode.postValue(ErrorCodeUtil.getErrorCode(e));
                     }
 
                     @Override
